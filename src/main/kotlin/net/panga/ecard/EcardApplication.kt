9b57dce3cab0fc3a971546f6fc1b8beee8f58bd2
dickode78 <dickode78@gmail.com>
2024-09-04 10:42:51 +0000

    Add security support

--- src/main/kotlin/net/panga/ecard/EcardApplication.kt
+++ src/main/kotlin/net/panga/ecard/EcardApplication.kt
@@ -1,9 +1,25 @@
 package net.panga.ecard
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@OpenAPIDefinition(
+    servers = [
+        io.swagger.v3.oas.annotations.servers.Server(
+            url = "http://localhost:8080"
+        ),
+        io.swagger.v3.oas.annotations.servers.Server(
+            url = "https://ecard-backend.dickode.net"
+        )
+    ],
+    info = io.swagger.v3.oas.annotations.info.Info(
+        title = "Ecard API",
+        version = "1.0",
+        description = "Ecard API Documentation"
+    )
+)
 class EcardApplication
 
 fun main(args: Array<String>) {
